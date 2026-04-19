f4580dc41747c638be39c687742ef4d4f0069e2b
paulogomet <pablogomezrobledano@gmail.com>
2026-04-19 13:46:02 +0200

    XNOR implementada con NANDs

---
+++ xnor_gate.vhd
@@ -0,0 +1,54 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xnor_gate is
+    port(
+        A_xnor : in STD_LOGIC;
+        B_xnor : in STD_LOGIC;
+        Z_xnor : out STD_LOGIC
+    );
+end xnor_gate;
+
+architecture Structural of xnor_gate is
+    component nand_gate is
+        Port(
+            A : in STD_LOGIC;
+            B : in STD_LOGIC;
+            Z : out STD_LOGIC
+        );
+    end component;
+
+    signal s1, s2, s3, s4 : STD_LOGIC;
+    
+begin 
+    U1: nand_gate port map (
+        A => A_xnor,
+        B => B_xnor,
+        Z => s1
+    );
+
+    U2: nand_gate port map (
+        A => A_xnor,
+        B => s1,
+        Z => s2
+    );
+
+    U3: nand_gate port map (
+        A => s1,
+        B => B_xnor,
+        Z => s3
+    );
+
+    U4: nand_gate port map (
+        A => s2,
+        B => s3,
+        Z => s4
+    );
+
+    U5: nand_gate port map (
+        A => s4,
+        B => s4,
+        Z => Z_xnor
+    );
+    
+end Structural;
